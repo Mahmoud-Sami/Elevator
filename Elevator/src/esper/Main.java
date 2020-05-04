f8f062214a5539e6286f1b07f4485b7b26d74def
Halim <>
2020-05-04 15:09:41 +0200

    Initial GUI

--- Elevator/src/esper/Main.java
+++ Elevator/src/esper/Main.java
@@ -1,6 +1,6 @@
 package esper;
 
-import view.GUI;
+import view.ElevatorGUI;
 
 public class Main {
     public static void main(String[] args){
--- Elevator/src/view/ElevatorGUI.java
+++ Elevator/src/view/ElevatorGUI.java
@@ -9,12 +9,12 @@ package view;
  *
  * @author halim
  */
-public class GUI extends javax.swing.JFrame {
+public class ElevatorGUI extends javax.swing.JFrame {
 
     /**
      * Creates new form GUI
      */
-    public GUI() {
+    public ElevatorGUI() {
         initComponents();
     }
 
@@ -486,20 +486,21 @@ public class GUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ElevatorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ElevatorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ElevatorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ElevatorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new GUI().setVisible(true);
+                new ElevatorGUI().setVisible(true);
             }
         });
     }
