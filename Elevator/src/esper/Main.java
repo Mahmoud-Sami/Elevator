5ea61abcd14a5d1a15fa4eeff3205ae3d713cb91
Halim <>
2020-05-04 15:03:53 +0200

    Initial GUI

--- Elevator/src/esper/Main.java
+++ Elevator/src/esper/Main.java
@@ -1,10 +1,9 @@
 package esper;
 
-import javafx.application.Application;
 import view.GUI;
 
 public class Main {
     public static void main(String[] args){
-        Application.launch(GUI.class, args);
+
     }
 }
