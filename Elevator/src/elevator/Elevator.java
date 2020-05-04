c41b6c14483be4ed5327d2a18f2ba05614f91fe5
Halim <>
2020-05-04 10:56:46 +0200

    Hello

--- Elevator/src/elevator/Elevator.java
+++ Elevator/src/elevator/Elevator.java
@@ -15,7 +15,7 @@ public class Elevator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Hello");
     }
     
 }
